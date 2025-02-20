3c8240df94fe6225334a65ea29d007dfc8d4caf3
loganWatson1400 <21watsol@gmail.com>
2025-02-20 13:37:51 -0600

    feat: modified the syntax of the model to use tf.keras.layers() format

--- make_model.py
+++ make_model.py
@@ -1,30 +1,26 @@
 import tensorflow as tf
-from keras.models import Sequential
-from keras.layers import GlobalAveragePooling2D, Dense, Dropout
-from keras.applications import Xception
-from keras.optimizers import Adam
 from data_process import prepare_data
 
-# testing
-from sklearn.metrics import classification_report, confusion_matrix
-import matplotlib.pyplot as plt
+### Parameters ###
+lr = 0.0001
+
 
 # Load dataset
 x_train, x_test, y_train, y_test = prepare_data()
 
 # Define the Xception-based CNN Model
 def build_xception_model():
-    base_model = Xception(weights="imagenet", include_top=False, input_shape=(224, 224, 3))
+    base_model = tf.keras.applications.Xception(weights="imagenet", include_top=False, input_shape=(224, 224, 3))
     base_model.trainable = False  # Freeze base 
 
-    model = Sequential([
+    model = tf.keras.models.Sequential([
         base_model,
-        GlobalAveragePooling2D(),
-        Dense(128, activation='relu'),
-        Dropout(0.5),  # Reduce overfit
-        Dense(7, activation='softmax')
+        tf.keras.laysers.GlobalAveragePooling2D(),
+        tf.keras.laysers.Dense(128, activation='relu'),
+        tf.keras.laysers.Dropout(0.5),  # Reduce overfit
+        tf.keras.laysers.Dense(7, activation='softmax')
     ])
 
-    model.compile(optimizer=Adam(learning_rate=0.0001), loss="categorical_crossentropy", metrics=["accuracy"])
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=lr), loss="categorical_crossentropy", metrics=["accuracy"])
 
     return model
