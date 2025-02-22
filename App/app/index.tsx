1c140442875f2f472c1c3fe042e396ba2f7343b2
HawkerX <jackharmer5@gmail.com>
2025-02-22 16:30:54 -0600

    feat: added basic hamburger drawer with screens
    Took way longer than it should've, but its there now :)

---
+++ App/app/index.tsx
@@ -0,0 +1,26 @@
+
+import React from 'react';
+import { createDrawerNavigator} from '@react-navigation/drawer';
+import Home from '@/screens/Home'
+import Info from '@/screens/Info';
+import Review from '@/screens/Review';
+import Settings from '@/screens/Settings';
+import {Ionicons} from '@expo/vector-icons';
+
+const Burger = createDrawerNavigator();
+
+export default function App() {
+    return (
+            <Burger.Navigator
+                screenOptions={{
+                    drawerIcon: ({ focused, size}) => (<Ionicons name='menu' size={size} color={focused ? '#7cc' : '#ccc'}/>),
+                }}
+            >
+                <Burger.Screen name='Home' component={Home} />
+                <Burger.Screen name='Info' component={Info} />
+                <Burger.Screen name='Review' component={Review} />
+                <Burger.Screen name='Settings' component={Settings} />
+            </Burger.Navigator>
+    );
+}
+
